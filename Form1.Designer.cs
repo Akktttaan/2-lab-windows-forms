b067a2748153d9d7f8fbdc668ff1ef5c9861b7a2
Akktttaan <iamaktanastarkulov@gmail.com>
2022-04-21 15:18:02 +0600

    some changes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -143,6 +143,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.ClientSize = new System.Drawing.Size(626, 258);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
